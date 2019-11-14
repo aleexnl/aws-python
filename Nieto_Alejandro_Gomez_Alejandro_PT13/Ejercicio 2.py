cd45e460c62b2b0001a6565816af88b4d2f695bd
aleexnl <alex.nieto0027@gmail.com>
2019-11-14 21:27:04 +0100

    Ejercicio 2 practica 13

--- Nieto_Alejandro_Gomez_Alejandro_PT13/Ejercicio 2.py
+++ Nieto_Alejandro_Gomez_Alejandro_PT13/Ejercicio 2.py
@@ -1,8 +1,10 @@
-lst = []
-psc = int(input("Introduce posicion: "))
-qty = int(input("Introduce cantidad de nombres a introducir: "))
-for i in range(qty):
-    lst.append(input("Introduce un nombre: "))
-i = psc
-for i in range(len(lst)):
-    print("Querido", lst[i], "me alegro de verte")
+lst = []  # Iniciamos lista
+psc = int(input("Introduce posicion: "))  # Pedimos valor
+qty = int(input("Introduce cantidad de nombres a introducir: "))  # Pedimos cantidad
+'''Este bucle lo que hara es pedir tantos nombres como se le ha indicado en el input anterior.'''
+for i in range(qty):  # Iniciamos bucle
+    lst.append(input("Introduce un nombre: "))  # Pedimos un valor y lo indexamos en una lista
+'''Este bucle lo que hará es empezar a imprimir los nombres a partir de la posicion que se ha 
+indicado basandose en el largo de la lista'''
+for psc in range(len(lst)):
+    print("Querido", lst[psc], "me alegro de verte")
