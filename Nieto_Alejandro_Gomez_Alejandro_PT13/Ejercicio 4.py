6737e014c3a5fb67f9878f5d7c98fe344950e8a8
Alejandro Nieto <alex.nieto0027@gmail.com>
2019-11-15 13:13:56 +0100

    Ejercicio 4 Practica 13

--- Nieto_Alejandro_Gomez_Alejandro_PT13/Ejercicio 4.py
+++ Nieto_Alejandro_Gomez_Alejandro_PT13/Ejercicio 4.py
@@ -20,19 +20,33 @@ for j in range(len(lst)):  # Iniciamos el bucle
     total += lst[j]  # Hacemos la suma de VALOR + NUM.POSICION LISTA
 print("Suma de todos los valores: ", total)  # Mostramos el total de la suma
 print("Media de los valores: ", total / len(lst))  # Hacemos el calculo de la media y lo mostramos
-'''Este bucle hara la descomposicion factorial de todos los numeros en la lista, a excepcion de el 0, 1 y 3 que no
-tienen divisores mas que ellos mismos'''
-for k in range(len(lst)):  # Iniciamos el bucle
-    factory = []  # Iniciamos la lista de factoriales
-    dvsr = 2  # Iniciamos el numero divisor inicial
-    while True:  # Iniciamos un bucle anidado
-        if lst[k] % dvsr == 0:  # Si el numero es divisor...
-            factory.append(dvsr)  # Añade el numero a la lista
-            lst[k] = lst[k] / dvsr  # Cambia el valor de la lista
-        if lst[k] % dvsr != 0:  # Si no es divisor...
-            dvsr += 1  # Cambiamos directamente el valor de i
-        if lst[k] == dvsr:  # Si el divisor es igual que el numero
-            factory.append(dvsr)  # Lo añade a la lista
-            break # Paramos el bucle anidado
-    print("Descomposicion factorial :", factory)  # Mostramos la lista de la descompsoicion factorial de el numero
+'''Este bucle hara el factorial de cada numero y lo apendara una lista, de tal manera que cuando acabe el bucle este lo
+que hara es mostrar todos los factoriales de cada numero.'''
+for k in range(len(lst)):
+    factorynum = [lst[k]]
+    timer = lst[k]
+    for l in range(lst[k]):
+        if timer - 1 == 0:
+            break
+        factorynum[0] = factorynum[0] * (timer - 1)
+        timer -= 1
+    factory.append(factorynum[0])
+print("Factoriales de cada numero: ", factory)
+
+'''ESTA PARTE DE CODIGO HACE LA DESCOMPOSICION FACTORIAL DE CADA NUMERO EN LA LISTA Y LUEGO AL MUESTRA. NO FUNCIONA CON
+EL 0, 1 Y 3'''
+# factory = []
+# for k in range(len(lst)):  # Iniciamos el bucle
+#     factory = []  # Iniciamos la lista de factoriales
+#     dvsr = 2  # Iniciamos el numero divisor inicial
+#     while True:  # Iniciamos un bucle anidado
+#         if lst[k] % dvsr == 0:  # Si el numero es divisor...
+#             factory.append(dvsr)  # Añade el numero a la lista
+#             lst[k] = lst[k] / dvsr  # Cambia el valor de la lista
+#         if lst[k] % dvsr != 0:  # Si no es divisor...
+#             dvsr += 1  # Cambiamos directamente el valor de i
+#         if lst[k] == dvsr:  # Si el divisor es igual que el numero
+#             factory.append(dvsr)  # Lo añade a la lista
+#             break # Paramos el bucle anidado
+#     print("Descomposicion factorial :", factory)  # Mostramos la lista de la descompsoicion factorial de el numero
 
