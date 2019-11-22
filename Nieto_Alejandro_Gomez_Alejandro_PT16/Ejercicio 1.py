0cea7e5043f8c012d6c6d4382dc37bf33d8c0448
Alejandro Nieto <alex.nieto0027@gmail.com>
2019-11-22 17:01:08 +0100

    Ejercicio 1 Practica 16

--- Nieto_Alejandro_Gomez_Alejandro_PT16/Ejercicio 1.py
+++ Nieto_Alejandro_Gomez_Alejandro_PT16/Ejercicio 1.py
@@ -1,6 +1,8 @@
 dicc = {}  # Definimos un diccionario
 phrase = str(input("Introduce una frase: "))  # Pedimos un valor en formato texto
 wrds = phrase.split()  # Creamos una lista donde se almacenarán todas las palabras de la variable anterior
-for i in range(len(wrds)):  
+'''Este bucle lo que hara es pasar por todas las posiciones de la lista segun su longitud'''
+for i in range(len(wrds)):  # Iniciamos bucle
+    '''Asigna la palabra en posicion i en la clave y la  lognitud de la palabra i en el valor segun su longitud'''
     dicc[wrds[i]] = len(wrds[i])
-print(dicc)
+print(dicc)  # Mostramos el diccionario
