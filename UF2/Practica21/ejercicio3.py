af8a52c56816853da2404374027a86cb6e960284
bycarlos28 <carlosfernandez28correa@gmail.com>
2019-12-11 08:17:33 +0100

    ejercicio 5 practica 21

--- UF2/Practica21/ejercicio3.py
+++
@@ -1,9 +0,0 @@
-def alfabetica(paraula):
-    paraula.isalpha()
-    if paraula.isalpha() == True:
-        print('La paraula, es alfabetica')
-    else:
-        print('La paraula, es no alfabetica')
-
-hola = input('Introduce una palabra:')
-alfabetica(hola)
