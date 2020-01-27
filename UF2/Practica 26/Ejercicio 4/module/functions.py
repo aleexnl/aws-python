789d27903122b936312a83e91792ecca3f8a11ee
Alejandro Nieto <alex.nieto0027@gmail.com>
2020-01-27 10:32:06 +0100

    UF3: Practica 28

---
+++ UF2/Practica 26/Ejercicio 4/module/functions.py
@@ -0,0 +1,10 @@
+""" Donat un vector de números enters ordenat decreixentment, dissenyeu un programa recursiu que comprovi si el valor
+d’algun dels elements del vector coincideix amb el seu índex. """
+
+def sum_list_recursive(list):
+    if list[] ==
+
+
+vector = [10, 5, 1, -1, -5, -10]
+print(sum_list_recursive(vector))
+
