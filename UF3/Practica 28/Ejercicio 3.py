dd643bd34997074cbf42542346e1be4c18d83379
rohan241 <57097506+rohan241@users.noreply.github.com>
2020-01-29 08:45:54 +0100

    Ejercicio 3 Practica 28

--- UF3/Practica 28/Ejercicio 3.py
+++ UF3/Practica 28/Ejercicio 3.py
@@ -1,10 +1,21 @@
+#creamos una lista para la informaicon que queramos separar con delimitador
 lista = ["nombre", "fecha", "horas", "signatura"]
+# Creamos una variable "archivo" para abrir el arahcivo con txt y ponemos "r" que
+# significa modo lectura
 archivo = open("ejercicio.txt","r")
+# Creamos una variable "delimitador", que nos sirvira separar la informacion del
+# archivo y de la lista mediante dos puntos
 delimitador = " : "
+# Creamos una variable contador para leer todas las lineas del arhivo con readline
 contador = len(archivo.readline())
+# seek sirve para situar en punto inicio mostrando en la posicion 0
 archivo.seek(0)
+# Con este bucle pasaremos por toda la informacion
 for i in range(1, contador-1):
+# Finalmente printamos en la pantalla la información de la lista separado con el
+# delimitador junto con las lineas del archivo.
     print(lista[i-1], delimitador, archivo.readline())
+# Finalmente cerramos el archivo
 archivo.close()
 
 
--- UF3/Practica 28/ejercicio4.py
+++ UF3/Practica 28/ejercicio4.py
@@ -1,14 +1,18 @@
+# abrivos el archivo de modo lectura con "r"
 archivo = open("ejercicio4.txt", "r")
+# Creamos varables lineas, palabras, caracters con el valor inicialmente 0
 lineas = 0
 palabra = 0
 caracters = 0
+# creamos un bucle que nos pasara por todas las lineas del archivo
 for i in archivo.readlines():
     lineas += 1
     caracters += len(i)
     texto =i.split()
     palabra += len(texto)
+# Finalmente printamos las variables para que nos printe todas lineas, palabras
+# y caracters que tiene el arhivo,
 print("Tiene", lineas ,"lineas")
 print("Tiene",palabra, "Palabras")
 print("Tiene",caracters, "Caracteres")
 archivo.close()
-
