3aaa856f1029117fc8a5d3e2d2b83aab2a5d1571
alejandro <alex.nieto0027@gmail.com>
2020-02-24 14:59:07 +0100

    Practica 36: Ejercicio 1; Añadir comentarios

--- UF4/Practica 36/src/Ejercicio1.java
+++ UF4/Practica 36/src/Ejercicio1.java
@@ -7,8 +7,8 @@ public class Ejercicio1 {
 		Scanner lector = new Scanner(System.in);  // Definimos variable para lector de teclado
 		System.out.print("Introduce un numero para calcular el cuadrado de este: ");  // Mostramos mensaje
 		int numero = lector.nextInt();  // Asignaos el valor introducido por teclado a la variable
-		int resultat = numero*numero;
-		System.out.println("El cuadrado de " + numero + " es: " + resultat);
+		int resultat = numero*numero;  // Hacemos el calculo de el cuadrado y lo guardamos en una variable
+		System.out.println("El cuadrado de " + numero + " es: " + resultat);  // Mostramos resultado con numero personalizado
 		
 	}
 
