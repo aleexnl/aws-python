b4f2585e409c43aa59e42b30b3817f0148ccca8e
Alejandro Nieto <alex.nieto0027@gmail.com>
2020-02-25 11:06:15 +0100

    Added @SuppressWarnings

--- UF4/Practica 36/src/Ejercicio1.java
+++ UF4/Practica 36/src/Ejercicio1.java
@@ -4,6 +4,7 @@ public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		@SuppressWarnings("resource")
 		Scanner lector = new Scanner(System.in);  // Definimos variable para lector de teclado
 		System.out.print("Introduce un numero para calcular el cuadrado de este: ");  // Mostramos mensaje
 		int numero = lector.nextInt();  // Asignaos el valor introducido por teclado a la variable
