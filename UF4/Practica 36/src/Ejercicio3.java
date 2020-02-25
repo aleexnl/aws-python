88ea94180111f5c4d5f84aaaa08f513f65fb9ab3
TinfoilBat <cuentas.mantecon@gmail.com>
2020-02-25 19:43:50 +0100

    Finished P36

--- UF4/Practica 36/src/Ejercicio3.java
+++ UF4/Practica 36/src/Ejercicio3.java
@@ -1,33 +1,66 @@
 import java.util.Scanner;;
+
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int b;
-		int h;
-		boolean sortir;
+		int b = 0;
+		int h = 0;
+		boolean sortir = false;
 		Scanner lector = new Scanner(System.in);
-		
-		
-		do {} while(sortir == False);
-		
-		System.out.println("Gimme b");
-		
+
+
+		do {
+			do {
+				System.out.println("Dame b");
+
+				if (lector.hasNextInt()){
+					b = lector.nextInt();
+					sortir = true;
+
+				} else {
+					System.out.println("Error");
+
+				}
+			} while (!sortir);
+			
+			sortir = false;
+
+
+			do {
+				System.out.println("Dame h");
+
+				if (lector.hasNextInt()){
+					h = lector.nextInt();
+					sortir = true;
+				}
+
+				else {
+					System.out.println("Error");
+				}
+			} while(!sortir);
+
+
+
+		} while(!sortir);
+
+
+		System.out.println(perimeter(b, h));
+		System.out.println(area(b, h));
 		
 		
 	}
-	
+
 	public static float perimeter(int b, int h) {
-		float perimeter = 2*b + 2*h;
+		float perimeter = 2 * b + 2 * h;
 		return perimeter;
-		
-		
+
 	}
-	
+
 	public static float area(int b, int h) {
-		int area = 	b * h;
+		int area = b * h;
 		return area;
-		
+
 	}
 
 }
